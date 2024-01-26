8ac54e8a2a48da74e4240bd95b9b6860c9bc4fae
Crowdbotics <team@crowdbotics.com>
2024-01-26 16:53:04 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "therapy-for-you-45572",
+  url: "https://therapy-for-you-45572.botics.co",
+  api: "https://therapy-for-you-45572.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
